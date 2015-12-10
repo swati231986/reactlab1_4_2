d3752be583c7b10750bc0fb47cc7cfcb18068796
jgajula <jagdish_gajula@intuit.com>
2015-12-10 11:41:13 -0800

    Checking in name.js

---
+++ dev/js/components/name.js
@@ -0,0 +1,14 @@
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var NameRecord = React.createClass({
+  render:function(){
+    return (
+    	<div className="NameRecord"
+    		<input type="Text" placeholder="Name" name="Name"/>
+    		</div>
+    )}
+});
+
+module.exports = NameRecord;	
